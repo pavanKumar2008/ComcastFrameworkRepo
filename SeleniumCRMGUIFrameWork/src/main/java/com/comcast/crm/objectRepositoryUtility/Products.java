39c04536ee52085a5cc17111ae51736e3ced5d6d
jyoti <jyoti@LAPTOP-LPR232VR>
2024-06-25 16:56:06 +0530

    huiop

--- SeleniumCRMGUIFrameWork/src/main/java/com/comcast/crm/objectRepositoryUtility/Products.java
+++ SeleniumCRMGUIFrameWork/src/main/java/com/comcast/crm/objectRepositoryUtility/Products.java
@@ -2,7 +2,10 @@ package com.comcast.crm.objectRepositoryUtility;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-
+/**
+ * @author qwertyu
+ * 
+ */
 public class Products {
 	@FindBy(xpath = "//img[@title='Create Product...']")
 	private WebElement createProductImgBtn;
